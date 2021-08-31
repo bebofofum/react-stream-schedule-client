f411914813f9a661b1a9232999e267455edd83c1
Brian Boehm <bri.boehm@gmail.com>
2021-08-30 20:48:18 -0400

    Modifying app.js for handling routes.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import StreamsContainer from './containers/StreamsContainer';
+import AddStreamForm from './containers/AddStreamFormContainer';
+
+import NavBar from './components/Navbar'
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1> 
+     <Router>
+       <NavBar />
+
+       <Switch>
+         <Route
+          exact path="/">
+             <StreamsContainer />
+         </Route>
+         <Route
+          exact path="streams/new">
+           <AddStreamForm />
+         </Route>
+
+       </Switch>
+      </Router> 
     </div>
   );
 }
