07435e6b7204725686fb8c2ea9fa8f78f8407800
Brian Boehm <bri.boehm@gmail.com>
2021-08-30 20:47:31 -0400

    Adding containers

---
+++ src/containers/AddStreamFormContainer.js
@@ -0,0 +1,14 @@
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
+
+class AddStreamForm extends Component {
+    render() {
+        return (
+            <section>
+                Hi, I'm the Form.
+            </section>
+        )
+    }
+}
+
+export default AddStreamForm
---
+++ src/containers/StreamsContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class StreamsContainer extends Component {
+    render() {
+        return (
+            <section>
+                <h1>Hi I'm the Stream container</h1>
+            </section>
+        )
+    }
+}
+
+export default StreamsContainer
