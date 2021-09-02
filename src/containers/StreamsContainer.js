27cbbe8778e2f3c0e4a1e91e559c6abfd7d34795
Brian Boehm <bri.boehm@gmail.com>
2021-09-02 09:35:47 -0400

    Container to handle state and show the stream components, pass props

--- src/containers/StreamsContainer.js
+++ src/containers/StreamsContainer.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
+import StreamsList from '../components/StreamsList';
 
 
 class StreamsContainer extends Component {
+
+    state = {
+        streams: [],
+        loading: true
+    }
+
+    componentDidMount() {
+        this.setState({
+            streams: [{
+                name: "FirstStream",
+                description: "This is a first stream",
+                schedule: "Monday"
+            }, {
+                name: "SecondStream",
+                description: "A second stream",
+                schedule: "Thursday"
+            }],
+            loading: false
+        })
+    }
+
+
     render() {
         return (
-            <section>
-                <h1>Hi I'm the Stream container</h1>
+            <section className="section is-medium bg-yellow-50">
+                <div className="container is-max-desktop">
+                 <h1 className="">Hi I'm the Stream container</h1>
+                 {this.state.loading ? "loading spinner" : (<StreamsList streams={this.state.streams} />) }
+                 
+                </div>
             </section>
         )
     }
