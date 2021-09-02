9c471bb27a24392d6c74888c9f16cddc0b69a50d
Brian Boehm <bri.boehm@gmail.com>
2021-09-02 11:25:54 -0400

    Passed props, destructured, and created list items to show details.

--- src/components/StreamListItem.js
+++ src/components/StreamListItem.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const StreamListItem = ({ description, name }) => {
+const StreamListItem = ({ stream: { id, description, name } }) => {
     return(
         <div>
             <div>
                 <h3>Imma list item</h3>
-                <li>{description}</li>
+                <li className="" key={id}>{name}: {description}</li>
             </div>
 
         </div>
