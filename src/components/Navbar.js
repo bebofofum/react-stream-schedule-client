6dac701c483a94d04e9f802fae3fc94cc6440ff1
Brian Boehm <bri.boehm@gmail.com>
2021-09-07 12:23:27 -0400

    working out navigation using react router and Navlink components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,11 +6,15 @@ class NavBar extends Component {
     render() {
         return (
             <div>
-                <nav className="flex w-full justify-between text-center bg-gray-200 text-white p-3 space-x-2">
+                <nav className="flex w-full justify-between text-center bg-gray-200 text-white p-4 space-x-2">
                     <div>
                         <NavLink 
                         to="/"
                         exact>Home          
+                        </NavLink>
+                        <NavLink 
+                        to="/stream/new"
+                        exact>Add Stream         
                         </NavLink>
                     </div>
                 </nav>
