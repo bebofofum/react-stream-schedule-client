2ab3d57ec86f10b9e75d979623e687f9a1c2298c
Brian Boehm <bri.boehm@gmail.com>
2021-09-17 16:00:32 -0400

    will be used to house the stream details page

--- src/components/StreamListItem.js
+++ src/components/StreamListItem.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const StreamListItem = ({ stream: { id, description, name } }) => {
     return(
         <div>
             <div>
                 <h3>Imma list item</h3>
-                <li className="" key={id}>{name}: {description}</li>
+                <li className="" key={id}><Link to={`/streams/${id}`}>{name}:</Link> {description}</li>
             </div>
 
         </div>
