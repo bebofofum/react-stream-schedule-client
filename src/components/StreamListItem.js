90da4aa3da8f581c311ab878465b26cbe5596bec
Brian Boehm <bri.boehm@gmail.com>
2021-10-01 10:53:23 -0400

    Adding styling

--- src/components/StreamListItem.js
+++ src/components/StreamListItem.js
@@ -3,13 +3,17 @@ import { Link } from 'react-router-dom';
 
 const StreamListItem = ({ stream: { id, description, name } }) => {
     return(
-        <div>
-            <div>
+        <section className="relative pt-12 bg-blueGray-50">
+            <div className="items-center flex flex-wrap">
                 <h3>Imma list item</h3>
+                <div className="w-full md:w-4/12 ml-auto mr-auto px-4">
+                    
+
+                </div>
                 <li className="" key={id}><Link to={`/streams/${id}`}>{name}:</Link> {description}</li>
             </div>
 
-        </div>
+        </section>
     )
 }
 
--- src/components/StreamsList.js
+++ src/components/StreamsList.js
@@ -3,8 +3,8 @@ import StreamListItem from './StreamListItem';
 
 const StreamsList = ({ streams }) => {
     return (
-        <div>
-            <h1>I'm the Stream Listing</h1>
+        <div className="relative pt-12 bg-blueGray-50">
+            <h1 className="text-gray-800 text-3xl font-semibold">I'm the Stream Listing</h1>
             <ul>{streams.map(stream => <StreamListItem stream={stream} />)}</ul>
 
         </div>
