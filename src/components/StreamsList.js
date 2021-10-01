c43ab4d8632752d5cf6dd8ad97a37a4716e8aba2
Brian Boehm <bri.boehm@gmail.com>
2021-10-01 13:57:40 -0400

    Adding graphic url

--- src/components/StreamsList.js
+++ src/components/StreamsList.js
@@ -5,7 +5,7 @@ const StreamsList = ({ streams }) => {
     return (
         <div className="relative pt-12 bg-blueGray-50">
             <h1 className="text-gray-800 text-3xl font-semibold">I'm the Stream Listing</h1>
-            <ul>{streams.map(stream => <StreamListItem stream={stream} />)}</ul>
+            <ul>{streams.map(stream => <StreamListItem stream={stream.attributes} />)}</ul>
 
         </div>
     )
