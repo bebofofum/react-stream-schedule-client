4228faad60b0b76dc0ee55d058483a57b88e267f
Brian Boehm <bri.boehm@gmail.com>
2021-10-09 21:45:36 -0400

    Constructing the combineReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import streamsReducer from './streams';
 
-
+//This combineReducer then takes in our individual reducers as functions and assigns their return value to an object
 
 export default combineReducers({
+    streams: streamsReducer
 
 })
