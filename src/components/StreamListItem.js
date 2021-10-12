784dbf72f5e9434d50088763ded699ee443598a3
Brian Boehm <bri.boehm@gmail.com>
2021-10-11 21:47:14 -0400

    Styling and organizing

--- src/components/StreamListItem.js
+++ src/components/StreamListItem.js
@@ -1,32 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/StreamListItem.css'
 
 const StreamListItem = ({ stream: { id, description, name, schedule, stream_graphic_url } }) => {
     return(
-        <section className="relative pt-12 bg-blueGray-50">
-            <div className="items-center flex flex-wrap">
-                <h3>Imma list item</h3>
-                <div className="w-full md:w-4/12 ml-auto mr-auto px-4">
-                    
-
-                </div>
-                <li className="" key={id}>
-                     <figure className="">
+        <section className="w-full">
+            <div className="flex bg-white p-6 rounded-lg shadow-lg">
+                <div className="flex items-center md:px-2 lg:w-1/2">
+                    <li className="" key={id}>
+                        <figure className="w-full h-auto">
                         <img 
-                            className="w-full h-full rounded-md object-cover" 
+                            className="w-45 rounded-lg" 
                             src={stream_graphic_url} 
                             alt={name} /> 
-                    </figure>
-
-
-                    <div>
-                        <Link to={`/streams/${id}`}>{name}:</Link> {description}
+                        </figure>
+                    </li>                   
+                </div>
+                <div className="lg:w-1/2 p-5 px-12">
+                        <h3 className="text-5xl font-semibold text-gray-800 md:text-6xl pb-6">
+                            <Link to={`/streams/${id}`}>{name}</Link> 
+                        </h3>
+                        <p className="mt-2 text-sm text-gray-500 md:text-lg pb-6">
+                            {description}
+                        </p>
+                        <h3 className="capitalize text-2xl font-semibold text-gray-800">Schedule:</h3>
+                        <h2 className="text-2xl font-semibold md:text-4xl text-blue-600">{schedule}</h2>
                     </div>
-                    <p>{schedule}</p>
-                    </li>
-                
-                   
+
             </div>
+            
 
         </section>
     )
